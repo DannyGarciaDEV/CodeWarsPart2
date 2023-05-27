f05c631a220b99aa48631223ce9d8264c867598f
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-05-27 14:00:00 +0100

    Create Phone Number

---
+++ 8kyu.js
@@ -0,0 +1,29 @@
+// Basic variable assignment
+// This code should store "codewa.rs" as a variable called
+//  name but it's not working. Can you figure out why?
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+
+
+
+// Keep Hydrated!
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+function litres(time) {
+    let calculateLitres = Math.floor(time * 0.5);
+    return calculateLitres;
+  }
