782c83726b99ef51a577b281416f2c9bd55951ca
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-05-28 14:12:20 -0400

    hello

--- main.js
+++ main.js
@@ -56,3 +56,66 @@ function highAndLow(numbers){
   numbers = numbers.split(" ");
   return Math.max(...numbers) +" "+ Math.min(...numbers);
 }
+
+
+//Disemvowel Trolls
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
+// Fizzbuzz
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+
+// Method calling example:
+
+// fizzbuzz(3) -->  [1, 2, "Fizz"]
+function fizzbuzz(n){
+
+  let arr = []
+  
+    for( var i=1; i<=n; i++){
+      if(i % 15 == 0){
+        arr.push('FizzBuzz')
+      }else if(i % 3 == 0){
+        arr.push('Fizz')
+      }else if(i % 5 == 0){
+        arr.push('Buzz')
+      }else{
+        arr.push(i)
+      }
+    }
+  
+    return arr
+  
+  }
+
+
+//you're a square!
+// A square of squares
+// You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+// Task
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
