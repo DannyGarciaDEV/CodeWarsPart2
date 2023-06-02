32200ef08e3b2cd1df40a27dc4e91bafad7dfe38
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-06-01 23:11:46 -0400

    Simple remove duplicates

--- main.js
+++ main.js
@@ -133,3 +133,20 @@ function isSquare(n) {
 function validatePIN (pin) {
   return /^(\d{4}|\d{6})$/.test(pin);
 }
+
+// Simple remove duplicates
+// Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+// Example:
+// For input: [3, 4, 4, 3, 6, 3]
+
+// remove the 3 at index 0
+// remove the 4 at index 1
+// remove the 3 at index 3
+// Expected output: [4, 6, 3]
+
+// More examples can be found in the test cases.
+
+function solve(arr){
+  return arr.filter((a,b) => arr.lastIndexOf(a) == b);
+}
