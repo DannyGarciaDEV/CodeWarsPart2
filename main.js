f904a42879cd8f9d384f26f6025936a6f85f3c9c
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-06-07 14:00:00 +0100

    Calculate Julie's Age

--- main.js
+++ main.js
@@ -166,3 +166,38 @@ function squareDigits(num){
   let  numNew = num.toString().split("").map(num => num * num).join("")
   return Number(numNew)
 }
+
+// Julie is x years older than her brother, and she is also y times as old as him.
+// Given x and y calculate Julie's age using the function age(x, y).
+// For example:
+// Age(6, 3) // returns 9
+// Note also that x can be negative, and y can be a decimal.
+// Age(-15, 0.25) // returns 5
+// That is, Julie is 15 years younger and 0.25 times the age of her brother.
+// Do not concern yourself with the imperfections inherent in dividing by floating point numbers, 
+// as your answer will be rounded. Also, for the sake of simplicity, Julie is never the same age as her brother.
+
+
+function age(x, y){
+  return x/(y-1)*y
+}
+
+
+//Cats in Hats
+// The Cat In The Hat has cat A under his hat, cat A has cat B under his hat and so on until Z
+
+// The Cat In The Hat is 2,000,000 cat units tall.
+
+// Each cat is 2.5 times bigger than the cat underneath their hat.
+
+// Find the total height of the cats if they are standing on top of one another.
+
+// Counting starts from the Cat In The Hat
+
+// n = the number of cats
+
+// fix to 3 decimal places.
+
+function height(n) {
+  return (2000000 * (1 - Math.pow(0.4, n + 1)) / (1 - 0.4)).toFixed(3);
+}
