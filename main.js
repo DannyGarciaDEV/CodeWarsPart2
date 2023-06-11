1b64513edae5ec4f36f633b8e7e6a99f1190696a
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-06-11 11:06:06 -0400

    Your commit message

--- main.js
+++ main.js
@@ -236,7 +236,7 @@ function chineseZodiac(year){
 
 
 // Multiples!
-// Make a program that takes a value (x) and returns "Bang" if the number is divisible by 3,
+// Make a program that takes a value (x) and returns "Bang" if the number is divisible by 3.
 //  "Boom" if it is divisible by 5, "BangBoom" if it divisible by 3 and 5, and "Miss"
 //   if it isn't divisible by any of them. Note: Your program should only return one value
 
