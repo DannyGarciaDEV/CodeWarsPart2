e9749501ad64b641c0184744f4428b3da4bebd6c
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-06-13 20:45:55 -0400

    Reverse words 7kyu

--- main.js
+++ main.js
@@ -273,5 +273,12 @@ function getSum(a,b){
         count += b;
       }
   }
-  return count;
+  return count
 }
+
+// Reverse words
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+  function reverseWords(str) {
+return str.split("").reverse().join("").split(" ").reverse().join(" ");
+  }
