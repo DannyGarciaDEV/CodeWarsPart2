4e578d72f1bf0770427f04f944db168e8f615ec4
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-06-22 21:07:32 -0400

    Create Phone Number

--- 6kyu.js
+++ 6kyu.js
@@ -61,3 +61,23 @@ function solution(roman) {
   
     return sum;
   }
+
+
+//   Create Phone Number
+//   Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+//   Example
+//   createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+
+  function createPhoneNumber(numbers) {
+    if (numbers.length !== 10) {
+      return "Invalid input. Please provide an array of 10 integers.";
+    }
+  
+    const areaCode = numbers.slice(0, 3).join('');
+    const firstPart = numbers.slice(3, 6).join('');
+    const secondPart = numbers.slice(6).join('');
+  
+    return `(${areaCode}) ${firstPart}-${secondPart}`;
+  }
