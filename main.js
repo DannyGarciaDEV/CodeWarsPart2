ab88ece9c2ee34c003105e21b07fe12fb12aa06f
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-07-16 16:04:01 -0400

    Vampire Numbers

--- main.js
+++ main.js
@@ -421,3 +421,18 @@ function waterbombs(fire, w) {
 function lastChair(n) {
   return n - 1;
 }
+
+// Vampire Numbers
+// Our loose definition of Vampire Numbers can be described as follows:
+
+// 6 * 21 = 126
+// # 6 and 21 would be valid 'fangs' for a vampire number as the 
+// # digits 6, 1, and 2 are present in both the product and multiplicands
+
+// 10 * 11 = 110
+// # 110 is not a vampire number since there are three 1's in the
+// # multiplicands, but only two 1's in the product
+
+var vampire_test = function(a, b){
+  return (''+a+b).split('').sort().join() == (a*b+'').split('').sort().join();
+}
