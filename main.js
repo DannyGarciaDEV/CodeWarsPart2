26bb4de63abd22d1daabd2196b8b3527ed1899d1
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-07-29 17:55:24 -0400

    basic method

--- main.js
+++ main.js
@@ -469,3 +469,18 @@ var vampire_test = function(a, b){
 //   doubleCheck("a!@€£#$%^&*()_-+=}]{[|\"':;?/>.<,~")
 //   //returns false
 const doubleCheck = (str) => /(.)\1/i.test(str);
+
+//Basi Method
+/*
+Description:
+Implement this
+
+[2,5,1,3].max() // returns 5
+[1,2,3,8,4,9,7,42,99].max() // returns 99
+[2,'5',1,3].max() // returns 5
+[2,5,1,'ab'].max() // returns NaN
+Array will contain at least one item.
+*/
+Array.prototype.max = function() {
+  return Math.max(...this);
+};
