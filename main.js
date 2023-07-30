bd4158f3212a65ce564158e8a49c085fa9509a78
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-07-30 18:01:05 -0400

    batman quotes

--- main.js
+++ main.js
@@ -484,3 +484,33 @@ Array will contain at least one item.
 Array.prototype.max = function() {
   return Math.max(...this);
 };
+
+// 7 kyu
+// Batman Quotes
+// 62884% of 334677 of 2,350afarcilla
+// JavaScript
+// TRAIN AGAINNEXT KATA
+// Details
+// Solutions
+// Discourse (46)
+// Translations
+// Fork|Collect|
+// DESCRIPTION:
+// Batman & Robin have gotten into quite a pickle this time. The Joker has mixed up their iconic quotes and also replaced one of the characters in their names, with a number. They need help getting things back in order.
+
+// Implement the getQuote method which takes in an array of quotes, and a string comprised of letters and a single number (e.g. "Rob1n") where the number corresponds to their quote indexed in the passed in array.
+
+// Return the quote along with the character who said it:
+
+// getQuote(["I am vengeance. I am the night. I am Batman!", "Holy haberdashery, Batman!", "Let's put a smile on that faaaceee!"], "Rob1n") 
+//   // =>  "Robin: Holy haberdashery, Batman!
+// Hint: You are guaranteed that the number in the passed in string is placed somewhere after the first character of the string. The quotes either belong to "Batman", "Robin", or "Joker".
+
+
+function getQuote(quotes, hero) {
+  return (
+    { R: "Robin", J: "Joker", B: "Batman" }[hero[0]] +
+    ": " +
+    quotes[hero.match(/\d+/)[0]]
+  );
+}
