fb2174f746ba0bb3b779204ac22314d321ec6b10
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-08-01 11:59:51 -0400

    vowel count

--- main.js
+++ main.js
@@ -514,3 +514,22 @@ function getQuote(quotes, hero) {
     quotes[hero.match(/\d+/)[0]]
   );
 }
+//Vowel Count
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i].toLowerCase(); // Convert to lowercase for case-insensitivity
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+      count++;
+    }
+  }
+
+  return count;
+}
