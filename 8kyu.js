739d086e92e8a22c7f43b64b6b2f500f6013f8da
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-08-11 11:23:22 -0400

    If you can't sleep, just count sheep

--- 8kyu.js
+++ 8kyu.js
@@ -136,6 +136,19 @@ function litres(time) {
 
     return result;
 }
+// If you can't sleep, just count sheep!!
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+var countSheep = function (num) {
+  if (num === 0) {
+    return "";
+  } else {
+    return countSheep(num - 1) + `${num} sheep...`;
+  }
+};
+
 
 /*
 We are living in communism and everyone has got christmas tree(same kind) from goverment!
