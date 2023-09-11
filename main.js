c7512e6229dc55e27d23c207b71336965dcb50c7
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-09-11 18:57:20 -0400

    a promise is a promise

--- main.js
+++ main.js
@@ -641,3 +641,12 @@ function stringsConstruction(str, target) {
     }
   }
 }
+
+
+/*
+Description:
+Create a resolved javascript Promise that will return 'Hello World!'.
+*/
+async function promiseHelloWorld() {
+  return await "Hello World!";
+}
