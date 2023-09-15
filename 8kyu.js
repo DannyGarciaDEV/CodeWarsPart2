0eb72308d478a2049a5213779be373a9beeba2a0
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-09-15 10:24:46 -0400

    hello

--- 8kyu.js
+++ 8kyu.js
@@ -183,3 +183,15 @@ function decorateTree() {
   console.log(tree);
   return tree;
 }
+
+
+/*
+Description:
+You task to pass only this tests :
+
+a == false
+!a == false
+a == !a
+*/
+
+const a = '0';
