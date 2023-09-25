093da565b02f28afecbfa1cf760a5ee699db2a73
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-09-25 11:07:23 -0400

    You Can't Code Under Pressure

--- 8kyu.js
+++ 8kyu.js
@@ -225,3 +225,11 @@ function squareSum(numbers){
     }
     return total
   }
+  // Code as fast as you can! You need to double the integer and return it.
+
+
+
+  function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
