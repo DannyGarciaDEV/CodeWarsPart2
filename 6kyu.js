d431b8ee0910e992f70dd9e28b48cbd927c804a0
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-10-05 12:42:20 -0400

    who likes it

--- 6kyu.js
+++ 6kyu.js
@@ -398,3 +398,32 @@ function roomMates(rooms, floor) {
   return rooms.slice(floor * 6 - 6, +floor * 6).filter(v => v);
 }
 // The worst kata
+
+//Who likes it?
+
+
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+function likes(names) {
+  if (names === null) {
+    return 'no one likes this';
+  } else if (names.length === 0) {
+    return 'no one likes this';
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]}, and ${names[2]} like this`;
+  } else {
+    return `${names[0]}, ${names[1]}, and ${names.length - 2} others like this`;
+  }
+}
