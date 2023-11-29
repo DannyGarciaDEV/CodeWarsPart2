8b8ec8a20697908dff541f34bcad8641a5799c68
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-11-29 13:13:25 -0500

    8kyu

---
+++ pythonCodewars/Convert Boolean values to String yes or no.py
@@ -0,0 +1,8 @@
+# 8 kyu
+# Convert boolean values to strings 'Yes' or 'No'.
+
+
+
+# DESCRIPTION:
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
