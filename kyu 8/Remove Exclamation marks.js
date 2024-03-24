5be2f1e0d0662f3e688509787d0d3325151561d4
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-03-23 20:29:56 -0400

    another way to rep;lace

--- kyu 8/Remove Exclamation marks.js
+++ kyu 8/Remove Exclamation marks.js
@@ -4,3 +4,18 @@ function removeExclamationMarks(s) {
     return s.replace('!', '')
     
     }
+
+    function replace(s) {
+        const vowels = 'aeiouAEIOU';
+        let result = '';
+        
+        for (let i = 0; i < s.length; i++) {
+          if (vowels.includes(s[i])) {
+            result += '!';
+          } else {
+            result += s[i];
+          }
+        }
+        
+        return result;
+      }
