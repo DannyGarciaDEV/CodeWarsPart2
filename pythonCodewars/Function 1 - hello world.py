513da5314d17e4d6c30dcbc854424d84114e0124
Danny Garcia <dannygarciaDEV@gmail.com>
2024-05-16 13:16:15 -0400

    8kyu

---
+++ pythonCodewars/Function 1 - hello world.py
@@ -0,0 +1,13 @@
+
+# Output
+# Past Solutions
+# Make a simple function called greet that returns the most-famous "hello world!".
+
+# Style Points
+# Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+def greet():
+    return "hello world!"
+
+# Example usage
+print(greet())
