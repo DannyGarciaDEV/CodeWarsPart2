7cf5c411c9c435501dba61610d54eb6fa2b38cb3
Danny Garcia <dannygarciaDEV@gmail.com>
2024-05-29 11:40:54 -0400

    7kyu

---
+++ pythonCodewars/254 shades of grey.py
@@ -0,0 +1,8 @@
+# Why would we want to stop to only 50 shades of grey? Let's see to how many we can go.
+
+# Write a function that takes a number n as a parameter and return an array containing n shades of grey in hexadecimal code (#aaaaaa for example). The array should be sorted in ascending order starting with '#010101', '#020202', etc. (using lower case letters).
+
+# Examples:
+
+# n =  1:    ["#010101"]
+# n = 10:    ["#010101", "#020202", "#030303", "#040404", "#050505", "#060606", "#070707", "#080808", "#090909", "#0a0a0a"]
