fdf9e88537c9c7087d5f5c3a195249dbe0a1b618
Danny Garcia <dannygarciaDEV@gmail.com>
2024-06-01 20:14:55 -0400

    8kyu

---
+++ pythonCodewars/Rock Paper Scissors!.py
@@ -0,0 +1,20 @@
+# Let's play! You have to return which player won! In case of a draw return Draw!.
+
+# Examples(Input1, Input2 --> Output):
+
+# "scissors", "paper" --> "Player 1 won!"
+# "scissors", "rock" --> "Player 2 won!"
+# "paper", "paper" --> "Draw!"
+def rps(p1, p2):
+    rules = {
+        "rock": "scissors",
+        "scissors": "paper",
+        "paper": "rock"
+    }
+    
+    if p1 == p2:
+        return "Draw!"
+    elif rules[p1] == p2:
+        return "Player 1 won!"
+    else:
+        return "Player 2 won!"
