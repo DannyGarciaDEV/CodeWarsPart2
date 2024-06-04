7bfd1a822543d8d9d6628a950b22bb37135c3e49
Danny Garcia <dannygarciaDEV@gmail.com>
2024-06-04 12:58:15 -0400

    7kyu

--- pythonCodewars/254 shades of grey.py
+++ pythonCodewars/254 shades of grey.py
@@ -6,3 +6,21 @@
 
 # n =  1:    ["#010101"]
 # n = 10:    ["#010101", "#020202", "#030303", "#040404", "#050505", "#060606", "#070707", "#080808", "#090909", "#0a0a0a"]
+
+def shades_of_grey(n):
+    """
+    Return an array containing n shades of grey in hexadecimal code.
+    
+    :param n: Number of shades to generate
+    :return: List of shades in hexadecimal format
+    """
+    if n <= 0:
+        return []
+    
+    shades = []
+    for i in range(1, min(n, 256)):  # Only up to 255 valid shades
+        hex_value = format(i, '02x')  # Convert to hexadecimal and ensure 2 characters
+        shade = f'#{hex_value}{hex_value}{hex_value}'  # Form the shade of grey
+        shades.append(shade)
+    
+    return shades
