a47925ba965a435dd0798424feb4db59f3ab6960
Danny Garcia <dannygarciaDEV@gmail.com>
2024-06-09 15:29:26 -0400

    7kyu

---
+++ pythonCodewars/Calculate Julie's Age.py
@@ -0,0 +1,21 @@
+#Julie is x years older than her brother, and she is also y times as old as him.
+
+#Given x and y calculate Julie's age using the function age(x, y).
+
+#For example:
+
+#age(6, 3) #returns 9
+#Note also that x can be negative, and y can be a decimal.
+
+#age(-15, 0.25) #returns 5
+#That is, Julie is 15 years younger and 0.25 times the age of her brother.
+
+#Do not concern yourself with the imperfections inherent in dividing by floating point numbers, as your answer will be rounded. Also, for the sake of simplicity, Julie is never the same age as her brother.
+
+def age(x, y):
+    # Calculate the brother's age
+    B = x / (y - 1)
+    # Calculate Julie's age
+    J = B + x
+    # Return the rounded Julie's age
+    return round(J)
