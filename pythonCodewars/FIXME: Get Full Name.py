b579d3bd968a9af80d5d5f3eac410a189293e0b2
Danny Garcia <dannygarciaDEV@gmail.com>
2024-06-17 14:54:30 -0400

    esier 7kyu

--- pythonCodewars/FIXME: Get Full Name.py
+++ pythonCodewars/FIXME: Get Full Name.py
@@ -8,22 +8,32 @@
 # Task
 # Fix the bug so we can all go home early.
 
+# class Dinglemouse(object):
+#     def __init__(self, first_name, last_name):
+#         self.first_name = first_name
+#         self.last_name = last_name
+        
+#     def get_full_name(self):
+#         # Remove leading and trailing spaces if any
+#         self.first_name = self.first_name.strip()
+#         self.last_name = self.last_name.strip()
+        
+#         # Check if either name is empty
+#         if not self.first_name or not self.last_name:
+#             return self.first_name + self.last_name
+        
+#         # Check if there's no space between first and last names
+#         if self.first_name[-1] != ' ' and self.last_name[0] != ' ':
+#             return self.first_name + ' ' + self.last_name
+#         else:
+#             return self.first_name + self.last_name
+        
+        # easier
+        
 class Dinglemouse(object):
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+       def __init__(self, first_name, last_name):
+        self.first_name = first_name.strip()
+        self.last_name = last_name.strip()
         
-    def get_full_name(self):
-        # Remove leading and trailing spaces if any
-        self.first_name = self.first_name.strip()
-        self.last_name = self.last_name.strip()
-        
-        # Check if either name is empty
-        if not self.first_name or not self.last_name:
-            return self.first_name + self.last_name
-        
-        # Check if there's no space between first and last names
-        if self.first_name[-1] != ' ' and self.last_name[0] != ' ':
-            return self.first_name + ' ' + self.last_name
-        else:
-            return self.first_name + self.last_name
+def get_full_name(self):
+        return f"{self.first_name} {self.last_name}".strip()
