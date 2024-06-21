336a47832f87782c2fd080c333a46c021e51f014
Danny Garcia <dannygarciaDEV@gmail.com>
2024-06-21 17:11:39 -0400

    8kyu

--- pythonCodewars/Get the mean of an array.py
+++ pythonCodewars/Get the mean of an array.py
@@ -5,3 +5,14 @@
 
 # The array will never be empty.
 
+def get_average(marks):
+    # Calculate the sum of all marks
+    total_sum = sum(marks)
+    
+    # Calculate the number of marks
+    count = len(marks)
+    
+    # Calculate the average and round down by using integer division
+    average = total_sum // count
+    
+    return average
