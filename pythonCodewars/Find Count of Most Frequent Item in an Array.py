ac2889a5b57e5963b69e5f54c79c763339bbb5dd
Danny Garcia <dannygarciaDEV@gmail.com>
2024-06-25 11:28:24 -0400

    7kyu

--- pythonCodewars/Find Count of Most Frequent Item in an Array.py
+++ pythonCodewars/Find Count of Most Frequent Item in an Array.py
@@ -6,3 +6,18 @@
 # ouptut: 5 
 # The most frequent number in the array is -1 and it occurs 5 times.
 
+def most_frequent_item_count(collection):
+    if not collection:
+        return 0
+    
+    frequency_dict = {}
+    
+    for item in collection:
+        if item in frequency_dict:
+            frequency_dict[item] += 1
+        else:
+            frequency_dict[item] = 1
+    
+    max_count = max(frequency_dict.values())
+    
+    return max_count
