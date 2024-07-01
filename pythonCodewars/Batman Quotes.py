5470e32b6618dbee605caf9e76ef453e9b6429a6
Danny Garcia <dannygarciaDEV@gmail.com>
2024-06-30 21:01:59 -0400

    7kyu

--- pythonCodewars/Batman Quotes.py
+++ pythonCodewars/Batman Quotes.py
@@ -9,3 +9,21 @@
 
 # BatmanQuotes.getQuote(["I am vengeance. I am the night. I am Batman!", "Holy haberdashery, Batman!", "Let's put a smile on that faaaceee!"], "Rob1n") should output =>  "Robin: Holy haberdashery, Batman!
 # Hint: You are guaranteed that the number in the passed in string is placed somewhere after the first character of the string. The quotes either belong to "Batman", "Robin", or "Joker".
+
+
+    @staticmethod
+    def get_quote(quotes, hero):
+        # Extract the index from the hero string
+        index = int(''.join(filter(str.isdigit, hero)))
+        
+        # Extract the position of the number
+        number_pos = next(i for i, c in enumerate(hero) if c.isdigit())
+        
+        # Correct the hero name by replacing the number with the correct letter
+        correct_hero_name = hero[:number_pos] + "i" + hero[number_pos+1:]
+        
+        # Retrieve the corresponding quote
+        quote = quotes[index]
+        
+        # Return the formatted string
+        return f"{correct_hero_name}: {quote}"
