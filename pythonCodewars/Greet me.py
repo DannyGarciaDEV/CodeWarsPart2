7e6ac7a7765bce29f92db4aaeefe410f21c6db9f
Danny Garcia <dannygarciaDEV@gmail.com>
2024-07-12 17:27:14 -0400

    7kyu

--- pythonCodewars/Greet me.py
+++ pythonCodewars/Greet me.py
@@ -4,3 +4,10 @@
 
 # "riley" --> "Hello Riley!"
 # "JACK"  --> "Hello Jack!"
+
+def greet(name):
+    # Capitalize the first letter of the name and make the rest lower case
+    capitalized_name = name.capitalize()
+    # Create the greeting message
+    greeting = f"Hello {capitalized_name}!"
+    return greeting
