0dc706ed006183b845dbe1d3906948f0fe70c05f
Danny Garcia <dannygarciaDEV@gmail.com>
2024-07-14 17:33:17 -0400

    7kyu

--- pythonCodewars/Power of 4.py
+++ pythonCodewars/Power of 4.py
@@ -3,3 +3,12 @@
 # (In C# Integer means all integer Types like Int16,Int32,.....)
 
 # Examples
+def powerof4(n):
+    # Check if n is less than or equal to 0, which cannot be a power of 4
+    if n <= 0:
+        return False
+    # Continuously divide n by 4
+    while n % 4 == 0:
+        n /= 4
+    # If the result is 1, then n is a power of 4
+    return n == 1
