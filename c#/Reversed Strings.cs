43a0206d241dfd24c366d7c759dc71597b195e31
Danny Garcia <dannygarciaDEV@gmail.com>
2024-09-15 11:27:23 -0400

    8kyu

--- c#/Reversed Strings.cs
+++ c#/Reversed Strings.cs
@@ -2,3 +2,15 @@
 
 // 'world'  =>  'dlrow'
 // 'word'   =>  'drow'
+
+using System;
+
+public static class Kata
+{
+    public static string Solution(string str) 
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
