261060c0e5a6f5cfa42eb5f5a2ba5cbeeb730f5b
Danny Garcia <dannygarciaDEV@gmail.com>
2024-10-01 11:50:40 -0400

    8kyu

--- Redoing js/Count of Positives sum of negatives.js
+++ Redoing js/Count of Positives sum of negatives.js
@@ -10,3 +10,22 @@
 function countPositivesSumNegatives(input) {
     // your code here
   }
+
+  function countPositivesSumNegatives(input) {
+    if (input === null || input.length === 0) {
+      return [];
+    }
+    
+    let countPositives = 0;
+    let sumNegatives = 0;
+    
+    input.forEach(num => {
+      if (num > 0) {
+        countPositives++;
+      } else if (num < 0) {
+        sumNegatives += num;
+      }
+    });
+    
+    return [countPositives, sumNegatives];
+  }
