0cd7a063d54d72216bd3c1751f94bebdc2ec59bb
Danny Garcia <dannygarciaDEV@gmail.com>
2024-10-13 14:41:46 -0400

    7kyu

---
+++ Redoing js/Sum of a sequence.js
@@ -0,0 +1,6 @@
+// he sequence is defined by 3 non-negative values: begin, end, step.
+
+// If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+const sequenceSum = (begin, end, step) => {
+ 
+};
