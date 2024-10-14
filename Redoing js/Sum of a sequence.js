148c9dcd4494aec6624bb32a1e809dc27bbe2976
Danny Garcia <dannygarciaDEV@gmail.com>
2024-10-14 14:42:25 -0400

    7kyu

--- Redoing js/Sum of a sequence.js
+++ Redoing js/Sum of a sequence.js
@@ -2,5 +2,13 @@
 
 // If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
 const sequenceSum = (begin, end, step) => {
- 
-};
+    if (begin > end) return 0;
+    
+    let sum = 0;
+    for (let i = begin; i <= end; i += step) {
+      sum += i;
+    }
+    
+    return sum;
+  };
+  
