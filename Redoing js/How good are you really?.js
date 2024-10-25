9884e0020a25922445e77b7ba0d91d20033820f4
Danny Garcia <dannygarciaDEV@gmail.com>
2024-10-25 12:04:06 -0400

    adding

---
+++ Redoing js/How good are you really?.js
@@ -0,0 +1,11 @@
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return true if you're better, else false!
+
+// Note:
+// Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+  }
+  
