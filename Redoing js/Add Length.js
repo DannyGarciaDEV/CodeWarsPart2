5014785ad7e87721d63c1b9c2e1c08edd9277a1c
Danny Garcia <dannygarciaDEV@gmail.com>
2024-11-16 13:00:09 -0500

    8kyu

--- Redoing js/Add Length.js
+++ Redoing js/Add Length.js
@@ -7,7 +7,10 @@
 // Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
 
 // Note: String will have at least one element; words will always be separated by a space.
-function addLength(str) {
-    //start-here
+
+    function addLength(str) {
+        // Split the string into an array of words, then map over the array to return each word with its length
+        return str.split(' ').map(word => `${word} ${word.length}`);
     }
+    
     
