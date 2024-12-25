09ddb1c7bd0bef14d788fa6dad5abc2f43723128
Danny Garcia <dannygarciaDEV@gmail.com>
2024-12-25 12:41:34 -0500

    8kyu

--- Redoing js/Sum Mixed Array.js
+++ Redoing js/Sum Mixed Array.js
@@ -1,3 +1,8 @@
 // Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
 // Return your answer as a number.
+function sumMix(x) {
+    // Convert all elements to numbers and calculate the sum
+    return x.reduce((sum, value) => sum + Number(value), 0);
+}
+
