539a072019e719bcea5fe8296fcbad916bcdc2bb
Danny Garcia <dannygarciaDEV@gmail.com>
2025-03-01 12:17:33 -0500

    pushing code

--- Redoing js/Break camelCase.js
+++ Redoing js/Break camelCase.js
@@ -4,3 +4,8 @@
 // "camelCasing"  =>  "camel Casing"
 // "identifier"   =>  "identifier"
 // ""             =>  ""
+
+
+function solution(string) {
+    return string.replace(/([a-z])([A-Z])/g, '$1 $2');
+  }
