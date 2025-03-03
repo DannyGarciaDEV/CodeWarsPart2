819057a6a6851f95aea6a925a6d6460a2de49e9f
Danny Garcia <dannygarciaDEV@gmail.com>
2025-03-03 12:18:42 -0500

    adding new solution

--- Redoing js/Break camelCase.js
+++ Redoing js/Break camelCase.js
@@ -9,3 +9,10 @@
 function solution(string) {
     return string.replace(/([a-z])([A-Z])/g, '$1 $2');
   }
+
+
+  const solution = string => {
+    return [...string].map((char) => {
+      return (char === char.toUpperCase()) ? ` ${char}` : char;
+    }).join('');
+  }
