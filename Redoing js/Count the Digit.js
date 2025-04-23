ad2a4673b499ef225388215ee69b329d2a98e78d
Danny Garcia <dannygarciaDEV@gmail.com>
2025-04-23 16:04:33 -0400

    7kyu

--- Redoing js/Count the Digit.js
+++ Redoing js/Count the Digit.js
@@ -33,3 +33,11 @@ function nbDig(n, d) {
     return count;
   }
   
+
+  function nbDig(n, d) {
+    var res=0;
+        for (var g=0;g<=n;g++){
+          var square=(g*g+"").split("");
+          square.forEach((s)=>s==d?res++:null)
+        }return res;
+    }
